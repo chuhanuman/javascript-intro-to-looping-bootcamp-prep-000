f35fb50d42cabb4dbd33d41b1c48f9fd5931091f
Hanuman Chu <chuhanuman@gmail.com>
2018-02-22 01:27:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(array) {
   for (let i=0;i<25;i++) {
       array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
-  
+  return array;
 }
 function maybeTrue() {
   return Math.random() >= 0.5;
@@ -17,4 +17,5 @@ function doWhileLoop(array) {
   do {
     array.pop();
   } while (maybeTrue() && array.length > 0);
+  return array;
 }
